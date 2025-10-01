e1a37f68407c6fa193593be52f0568e0ac2837ec
GamingG00N <gaminggoon31@gmail.com>
2025-10-01 13:38:06 -0400

    Add files via upload

--- #Hrvatin lab 2.3.py
+++ #Hrvatin lab 2.3.py
@@ -8,20 +8,38 @@ fname =name[0:space]
 lname = name[space+1 :len(name)]
 print(fname.title(),lname.title())
 
-#try to do this with less variables  hint concatenate the pieces together.
 difname =name .upper()
-lowerfname =difname[0].lower()
-upperfname =difname[1:space+1]
-lowerlname =difname[space+1].lower()
-upperlname =difname[space+2:len(difname)]
+newfname =difname[0].lower()+difname[1:space+1]
+newlname =difname[space+1].lower()+difname[space+2:len(difname)]
 
-print(lowerfname,upperfname,lowerlname,upperlname,"\n\n\n", sep="")
+print(newfname,newlname,"\n\n", sep="")
 
+print("Task 2")
 phrase =("Once you start down the dark path,"+\
         "forever will it dominate your destiny")
 
-newphrase =phrase.upper()
-newestphrase =newphrase.replace("1","1"),newphrase.replace("e","2"),newphrase.replace("i","3"),newphrase.replace("o","4"),newphrase.replace("u","5")
+newphrase =phrase.upper()\
+.replace("A","1")\
+.replace("E","2")\
+.replace("I","3")\
+.replace("O","4")\
+.replace("U","5")
 
-print(newestphrase)
-#i have no clue what im doing
+print(newphrase,"\n\n")
+
+
+print("Task 3")
+
+thirds =(int)(len(phrase)/3)
+phr1 =phrase[0:thirds]
+phr2 =phrase[thirds+1:thirds*2]
+phr3 =phrase[thirds*2+1:thirds*3]
+
+print(phr2,"\n",phr3,"\n",phr1,"\n\n",sep="")
+
+print("Task 4")
+
+number =(int)(input("Please enter five digits\n"))
+
+print(number[0]+number[1]+number[2]+number[3]+number[4])
+#i am pretty lost on this part
